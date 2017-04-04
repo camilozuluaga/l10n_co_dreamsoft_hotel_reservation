3f1f10d7478bb19f350e8fc60f080f445387a7f2
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-04-04 11:22:25 -0500

    agregando estructura basica para modulo de reserva

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Colombia - Reservacion',
+    'category': 'Localization',
+    'version': '1.0.0',
+    'author': 'DREAMSOFT',
+    'license': 'AGPL-3',
+    'maintainer': '',
+    'website': '',
+    'summary': 'Colombia Reservacion: Extended hotel_reservation'
+               'Contact Module - Odoo 10.0',
+    'images': [],
+    'depends': ['hotel_reservation'],
+    'data': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
